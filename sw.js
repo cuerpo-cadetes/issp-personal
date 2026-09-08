3455345a9796c77bcc8866e601c1cf394ff03034
cuerpo-cadetes <cuerpodecadetespcaba@gmail.com>
2026-09-08 16:45:48 -0300

    Actualizar caché estable de Personal a v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='issp-personal-v2';
+const CACHE='issp-personal-v3';
 const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
