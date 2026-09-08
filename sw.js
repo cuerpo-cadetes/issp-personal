09337a5a0976c3e839f88a66d6d9aa6dcd0d74d2
cuerpo-cadetes <cuerpodecadetespcaba@gmail.com>
2026-09-08 16:03:32 -0300

    Actualizar caché del acceso del personal

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-const CACHE='issp-personal-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener('fetch',event=>{if(new URL(event.request.url).origin===self.location.origin)event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));});
+const CACHE='issp-personal-v2';
+const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+self.addEventListener('activate',event=>event.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+    .then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',event=>{
+  if(new URL(event.request.url).origin!==self.location.origin)return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        if(response.ok&&event.request.method==='GET'){
+          const copy=response.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        }
+        return response;
+      })
+      .catch(()=>caches.match(event.request))
+  );
+});
